153e29a38fc8c15c59b89275cb3a1eab3520fe1a
rustiandacode <rustiandazen09@gmail.com>
2023-01-12 15:29:57 +0700

    added navbar section

---
+++ dist/js/script.js
@@ -0,0 +1,19 @@
+// navbar fixed
+window.onscroll = () => {
+  const header = document.querySelector('header');
+  const fixedNav = header.offsetTop;
+
+  if (window.pageYOffset > fixedNav) {
+    header.classList.add('navbar-fixed');
+  } else {
+    header.classList.remove('navbar-fixed');
+  }
+};
+
+// hamburger toggle
+const hamburger = document.querySelector('#hamburger');
+const navMenu = document.querySelector('#nav-menu');
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('hamburger-active');
+  navMenu.classList.toggle('hidden');
+});
